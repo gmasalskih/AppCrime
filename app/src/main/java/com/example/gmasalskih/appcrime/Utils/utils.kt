d06fc560ed484c6a501c676d98ddbbc672ade8ce
gmasalskih <gmasalskih@mail.ru>
2018-11-21 15:58:23 +0300

    begin chapter 16

--- app/src/main/java/com/example/gmasalskih/appcrime/Utils/utils.kt
+++ app/src/main/java/com/example/gmasalskih/appcrime/Utils/utils.kt
@@ -2,4 +2,4 @@ package com.example.gmasalskih.appcrime.Utils
 
 import java.util.*
 
-fun Date.toFormattedString()= android.text.format.DateFormat.format("dd.MM.yyyy hh:mm", this)
+fun Date.toFormattedString()= android.text.format.DateFormat.format("dd.MM.yyyy", this)
